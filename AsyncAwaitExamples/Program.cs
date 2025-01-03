8aa18136a28fed10d7e4e4645edcf57fcda4cafa
Michal Dmoch <m.dmoch.biz@gmail.com>
2025-01-03 18:58:00 +0100

    Add project files.

---
+++ AsyncAwaitExamples/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitExamples
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Zaczynamy przygotowanie śniadania!");
+
+            var smazenieBekonu = SmazBekonAsync(5);
+            var przygotowanieJajek = SmazJajeczniceAsync(3);
+
+            await Task.WhenAll(smazenieBekonu, przygotowanieJajek);
+
+            Console.WriteLine("Śniadanie gotowe! Smacznego!");
+        }
+
+        static async Task SmazBekonAsync(int iloscPlastrów)
+        {
+            Console.WriteLine($"Rozpoczynam smażenie {iloscPlastrów} plastrów bekonu...");
+            await Task.Delay(3000);
+            Console.WriteLine("Bekon jest gotowy!");
+        }
+
+        static async Task SmazJajeczniceAsync(int iloscJajek)
+        {
+            Console.WriteLine($"Rozbijam {iloscJajek} jajka i zaczynam smażenie...");
+            await Task.Delay(2000);
+            Console.WriteLine("Mieszanie jajek...");
+            await Task.Delay(2000);
+            Console.WriteLine("Jajecznica jest gotowa!");
+        }
+    }
+}
